4ba2f4f4935db95d2f06d9246daa2c6663cf8abb
Tolotra <tolotor@yahoo.fr>
2022-01-24 00:45:08 +0300

    Revamping dbConfig.ts #3

--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -1,26 +1,30 @@
-import { Dialect } from "sequelize/dist";
+import { Dialect } from "sequelize";
 
-export default {
-    development: {
-        username: "root",
-        password: "",
-        database: "api_posts",
-        host: "127.0.0.1",
-        dialect: "mysql" as Dialect,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    },
-    production: {
-        database_url: process.env.PROD_DATABASE_URL || '',
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+export const development = {
+    username: "root",
+    password: "",
+    database: "api_posts",
+    host: "127.0.0.1",
+    dialect: "mysql" as Dialect,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 };
+
+export const production = {
+    database_url: process.env.PROD_DATABASE_URL || '',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
+
+export default {
+    development,
+    production
+}
