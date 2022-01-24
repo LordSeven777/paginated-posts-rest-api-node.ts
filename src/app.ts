3bd37200a6aa6b0e3e9321a45b8d18aab34a93c5
Tolotra <tolotor@yahoo.fr>
2022-01-24 14:50:21 +0300

    Route for getting paginated posts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,31 @@
 import express from "express";
+import { Op } from "sequelize";
+
+// Post model
+import PostModel from "./model";
 
 // Express app
 const app = express();
 
-app.get('/', (req, res) => res.send('posts'));
+// Route for getting posts
+app.get('/', async (req, res) => {
+    const page = req.query.page ? parseInt(req.query.page as string) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
+    const before = req.query.before as string;
+
+    // Fetching posts from db
+    const posts = await PostModel.findAndCountAll({
+        where: !before ? {} : {
+            [Op.or]: [
+                { createdAt: { [Op.lte]: before } },
+                { updatedAt: { [Op.lte]: before } }
+            ]
+        },
+        offset: (page - 1) * limit,
+        limit
+    });
+
+    res.json(posts);
+});
 
 export default app;
